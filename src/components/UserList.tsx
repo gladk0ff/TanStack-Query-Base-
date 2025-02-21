819bd70acc9106961c5454847ce02a6ed38a6ee9
gladk0ff <sergladkov@outlook.com>
2025-02-21 22:01:23 +0300

    состояния placeholderData initialData

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,6 +1,8 @@
 import { keepPreviousData, useQuery } from "@tanstack/react-query";
 import { getUsers, IUserDto } from "../api/users";
 import { useState } from "react";
+import classNames from "classnames";
+// import { INITIAL_DATA } from "./initialData";
 
 export const UserList = () => {
   // isPending  нет данных , и нет запроса
@@ -12,6 +14,8 @@ export const UserList = () => {
     data: users,
     error,
     isPending,
+    isFetching,
+    isPlaceholderData,
   } = useQuery({
     // staleTime: Infinity,
     // gcTime: 1000,
@@ -19,7 +23,19 @@ export const UserList = () => {
     queryFn: ({ queryKey, signal }) =>
       getUsers({ page: queryKey[1] as number }, { signal }),
     placeholderData: keepPreviousData, // так же можно задать и функцию
+    // наполняет кеш  запроса из другого источника (localeStorage,somedata)
+    // очень полезен для ssr
+    // initialData: INITIAL_DATA,
   });
+
+  // сделать запрос на показ всех и пример посмотре с isFetching
+  // const { data: users5 } = useQuery({
+  //   // staleTime: Infinity,
+  //   // gcTime: 1000,
+  //   queryKey: ["users", 5],
+  //   queryFn: ({ queryKey, signal }) =>
+  //     getUsers({ page: queryKey[1] as number }, { signal }),
+  // });
 
   if (isPending) {
     return <p>Данные ожидаются</p>;
@@ -32,6 +48,8 @@ export const UserList = () => {
   const btnClass = "px-2 py-1  rounded cursor-pointer ";
   const btnAcions = btnClass + "bg-blue-200 hover:bg-blue-400";
   const btnPg = btnClass + "bg-gray-200 hover:bg-gray-400 ";
+  const btnDis = "disabled:pointer-events-none disabled:opacity-20";
+  console.log(page === users.pages);
 
   return (
     <section>
@@ -40,28 +58,30 @@ export const UserList = () => {
           <button className={btnAcions}>Добавить котика</button>
           <button className={btnAcions}>Включить</button>
         </div>
+        <p className="text-lg font-semibold">Страница : {page}</p>
         <div className="flex gap-2 dis">
           <button
             disabled={page === 1}
             onClick={() => setPage((p) => --p)}
-            className={
-              btnPg +
-              (page === 1
-                ? " disabled:pointer-events-none disabled:opacity-20"
-                : "")
-            }
+            className={classNames(btnPg, page === 1 && btnDis)}
           >
             {"<<"} Пред
           </button>
           <button
+            disabled={page === users.pages}
             onClick={() => setPage((p) => Math.min(++p, users.pages))}
-            className={btnPg}
+            className={classNames(btnPg, page === users.pages && btnDis)}
           >
             След {">>"}
           </button>
         </div>
       </div>
-      <ul className="bacground-color-gray flex flex-col  gap-2 mt-4">
+      <ul
+        className={classNames(
+          "bacground-color-gray flex flex-col  gap-2 mt-4",
+          isFetching && "opacity-50"
+        )}
+      >
         {users.data.map((user) => (
           <li key={user.id}>
             <UserListItem data={user} />
