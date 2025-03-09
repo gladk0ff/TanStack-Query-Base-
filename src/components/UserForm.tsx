5dfb9f0a131d17ef7cd85452adbb4cd632fab869
gladk0ff <sergladkov@outlook.com>
2025-03-09 20:38:13 +0300

    закоментировали что добавим потом

--- src/components/UserForm.tsx
+++ src/components/UserForm.tsx
@@ -1,62 +1,27 @@
-import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { QUERY_KEYS, usersQueries } from "../queries/users";
-import classNames from "classnames";
-
-const useCreateUser = () => {
-  const queryClient = useQueryClient();
-
-  return useMutation({
-    // mutationKey не обязателене если вы не хотите узнать статус мутации из другого компонента
-    mutationFn: usersQueries.createUser,
-    onSuccess: () =>
-      queryClient.invalidateQueries({
-        queryKey: [QUERY_KEYS.users],
-      }),
-    onError: (error) => {
-      // Обработка ошибки в мутации
-      console.log("Mutation error:", error);
-    },
-    onSettled: () => {
-      console.log("onSettled");
-    },
-  });
-};
+import { useMutation } from "@tanstack/react-query";
+import { usersQueries } from "../queries/users";
+// import classNames from "classnames";
 
 export const UserForm = ({ refetch }: { refetch?(): void }) => {
-  const queryClient = useQueryClient();
-
-  const { mutate, isPending, isError, error } = useCreateUser();
+  const { mutate } = useMutation({
+    // mutationKey не обязателене если вы не хотите узнать статус мутации из другого компонента
+    mutationFn: usersQueries.createUser,
+  });
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const formData = new FormData(e.currentTarget);
     //cинхронная не выбрасывает ошибки
-    mutate(
-      {
-        id: crypto.randomUUID(),
-        age: Number(formData.get("age")),
-        firstName: formData.get("firstName") as string,
-      },
-      {
-        onSuccess: () => {
-          queryClient.invalidateQueries({
-            queryKey: [QUERY_KEYS.users],
-          });
-          // queryClient.invalidateQueries(usersQueries.getUsersWithPagination()),
-        },
-        onError: (error) => {
-          // локальная обработка ошибки в компоненте
-          console.log("mutate local error:", error);
-        },
-      }
-    );
-
+    mutate({
+      id: crypto.randomUUID(),
+      age: Number(formData.get("age")),
+      firstName: formData.get("firstName") as string,
+    });
     e.currentTarget.reset();
   };
 
   return (
     <form className="flex gap-4" onSubmit={handleSubmit}>
-      {isError ? <div> {error.message}</div> : null}
       <input
         required
         name="firstName"
@@ -71,15 +36,56 @@ export const UserForm = ({ refetch }: { refetch?(): void }) => {
         type="number"
         placeholder="Возраст"
       />
-      <button
-        disabled={isPending}
-        className={classNames(
-          "px-2 py-1 rounded cursor-pointer bg-green-200 hover:bg-green-400",
-          isPending && "opacity-50"
-        )}
-      >
+      <button className="px-2 py-1 rounded cursor-pointer bg-green-200 hover:bg-green-400">
         Добавить
       </button>
     </form>
   );
 };
+
+// {isError ? <div> {error.message}</div> : null}
+
+{
+  /* <button
+disabled={isPending}
+className={classNames(
+  "px-2 py-1 rounded cursor-pointer bg-green-200 hover:bg-green-400",
+  isPending && "opacity-50"
+)}
+>
+Добавить
+</button> */
+}
+
+// const { mutate, isPending, isError, error } = useCreateUser();
+
+// const useCreateUser = () => {
+//   const queryClient = useQueryClient();
+
+//   return useMutation({
+//     // mutationKey не обязателене если вы не хотите узнать статус мутации из другого компонента
+//     mutationFn: usersQueries.createUser,
+//     onSuccess: () =>
+//       queryClient.invalidateQueries({
+//         queryKey: [QUERY_KEYS.users],
+//       }),
+//     onError: (error) => {
+//       // Обработка ошибки в мутации
+//       console.log("Mutation error:", error);
+//     },
+//     onSettled: () => {
+//       console.log("onSettled");
+//     },
+//   });
+// };
+
+// const queryClient = useQueryClient();
+
+// {
+//   onSuccess: () => {
+//     queryClient.invalidateQueries({
+//       queryKey: [QUERY_KEYS.users],
+//     });
+//     // queryClient.invalidateQueries(usersQueries.getUsersWithPagination()),
+//   },
+// }
